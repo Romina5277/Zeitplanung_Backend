cee7bd58354ebecb460ce4cc0dcfbc46d837302f
Romina Ferrario <saemyferrario@outlook.com>
2019-10-25 16:35:41 +0200

    DB-Connection aufgebaut (noch nicht getestet)

--- Zeitplanung_Backend/src/main/java/ch/axa/Zeitplanung_Backend/DAO/DAO.java
+++ Zeitplanung_Backend/src/main/java/ch/axa/Zeitplanung_Backend/DAO/DAO.java
@@ -1,4 +1,61 @@
 package ch.axa.Zeitplanung_Backend.DAO;
 
+import ch.axa.Zeitplanung_Backend.Objects.*;
+
+import javax.ejb.Singleton;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Singleton
 public class DAO {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<Termin> getTermine() {
+        TypedQuery<Termin> tq = em.createQuery(getSelectAllQuery("t", "Termin"), Termin.class);
+        List<Termin> termine = tq.getResultList();
+
+        return termine;
+    }
+
+    public List<Erinnerung> getErinnerungen() {
+        TypedQuery<Erinnerung> tq = em.createQuery(getSelectAllQuery("e", "Erinnerung"), Erinnerung.class);
+        List<Erinnerung> erinnerungen = tq.getResultList();
+
+        return erinnerungen;
+    }
+
+    public List<Label> getLabel() {
+        TypedQuery<Label> tq = em.createQuery(getSelectAllQuery("l", "Label"), Label.class);
+        List<Label> labels = tq.getResultList();
+
+        return labels;
+    }
+
+    public List<Notiz> getNotiz() {
+        TypedQuery<Notiz> tq = em.createQuery(getSelectAllQuery("n", "Notiz"), Notiz.class);
+        List<Notiz> notiz = tq.getResultList();
+
+        return notiz;
+    }
+
+    public List<ToDo> getToDo() {
+        TypedQuery<ToDo> tq = em.createQuery(getSelectAllQuery("t", "ToDo"), ToDo.class);
+        List<ToDo> todos = tq.getResultList();
+
+        return todos;
+    }
+
+
+    private String getSelectAllQuery(String kuerzel, String object) {
+        return "SELECT " + kuerzel + " FROM " + object + kuerzel;
+    }
+
+    private String getSelectSpezificQuery(String kuerzel, String object, int id) {
+        return "SELECT " + kuerzel + " FROM " + object + kuerzel + " WHERE `todo_id` = " + id;
+    }
+
 }
--- Zeitplanung_Backend/src/main/java/ch/axa/Zeitplanung_Backend/ZeitplanungBackendApplication.java
+++ Zeitplanung_Backend/src/main/java/ch/axa/Zeitplanung_Backend/ZeitplanungBackendApplication.java
@@ -1,13 +1,38 @@
 package ch.axa.Zeitplanung_Backend;
 
+import ch.axa.Zeitplanung_Backend.DAO.DAO;
+import ch.axa.Zeitplanung_Backend.Objects.Termin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ZeitplanungBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZeitplanungBackendApplication.class, args);
+
+		DAO dao = new DAO();
+		List<Termin> termine = dao.getTermine();
+
+		termine.forEach((termin -> {
+			System.out.println("--------------   Termin   ------------");
+			System.out.println("Name: " + termin.getName());
+			System.out.println("Kurzfassung: " + termin.getKurzfassung());
+			System.out.println("Von: " + termin.getVon());
+			System.out.println("Bis: " + termin.getBis());
+			System.out.println("Wo: " + termin.getWo());
+			System.out.println("Wer: " + termin.getWer());
+
+			if(termin.isGanztaegig()){
+				System.out.println("Ganztägig: ja");
+			} else {
+				System.out.println("Ganztägig: nein");
+			}
+
+			System.out.println("--------------------------------------");
+		}));
 	}
 
 }
